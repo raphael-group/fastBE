cd9ec00138d84c6f08468050f0f770bc5d90b784
Henri Schmidt <packerfan73@gmail.com>
2023-07-23 16:58:32 -0400

    Move Python code to scripts/ directory

--- scripts/vafpp_lp.py
+++ scripts/vafpp_lp.py
@@ -126,6 +126,7 @@ def one_vafpp_dp(tree, F):
     for j in range(F.shape[0]):
         f = one_vafpp_dual_recursive(j, 0)
         f = pl.compute_minimizer(f) + pl.PiecewiseLinear([1 - F[j, 0]], 0)
+        print(f)
         obj += np.min(f.intercepts)
         
     return -1 * obj
