942b85afb31bf828d9256db406679f39aa9a2233
Henri C. Schmidt <packerfan73@gmail.com>
2023-09-13 19:04:00 -0400

    Bug fix pairtree output parsing.

--- scripts/parse_pairtree_output.py
+++ scripts/parse_pairtree_output.py
@@ -1,12 +1,6 @@
 import numpy as np
 import argparse
 
-def convert_parents_to_adjmatrix(parents):
-    K = len(parents) + 1
-    adjm = np.eye(K)
-    adjm[parents,np.arange(1, K)] = 1
-    return adjm
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Parse pairtree output')
     parser.add_argument('result', type=str, help='Result file in NPZ format.')
@@ -14,17 +8,16 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     result = np.load(args.result)
-    best_tree_adj = convert_parents_to_adjmatrix(result['struct'][0])
-    best_tree_adj = best_tree_adj[1:, 1:]
+    best_tree_parents = result['struct'][0]
 
-    adjacency_list = [[] for _ in range(best_tree_adj.shape[0])]
-    for i in range(best_tree_adj.shape[0]):
-        for j in range(best_tree_adj.shape[1]):
-            if best_tree_adj[i, j] == 1:
-                adjacency_list[i].append(j)
+    print(best_tree_parents)
+    adjacency_list = [[i] for i in range(len(best_tree_parents) + 1)]
+    for i in range(len(best_tree_parents)):
+        adjacency_list[best_tree_parents[i]].append(i + 1)
+    print(adjacency_list)
 
     # write adjacency list to NX format
     with open(args.output, 'w') as f:
-        for i in range(len(adjacency_list)):
-            f.write(' '.join([str(x) for x in adjacency_list[i]]) + '\n')
+        for row in adjacency_list[1:]:
+            f.write(' '.join([str(x - 1) for x in row]) + '\n')
 
